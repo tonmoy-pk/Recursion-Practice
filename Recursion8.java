8522680b92139abc7165307f903fcb3d3d3b30b7
Tonmoy Kumar Pramanik <43000033+tonmoy-pk@users.noreply.github.com>
2022-09-30 03:14:06 +0600

    find all the permutation of a string

---
+++ Recursion8.java
@@ -0,0 +1,21 @@
+public class Recursion8 {
+    public static void main(String[] args) {
+        String str = "abcd";
+        printPerputation(str, "");
+    }
+    public static void printPerputation(String str, String newStr){
+        if(str.length()==0){
+            System.out.println(newStr);
+            return;
+        }
+        
+        for(int i=0;i<str.length();i++){
+            char currentchar = str.charAt(i);
+            String substring = str.substring(0, i)+str.substring(i+1);
+            //for the first time the substring is bc and a is added to the new string
+            //here we figure out substring without current char for next funtion call
+            printPerputation(substring, newStr+currentchar);
+            //in the first function call the substring is "bc" goes as str and newstr as "a"
+        }        
+    }
+}
